a67bf7742c7fb1d58e4552234169941837f26ee3
CRISTIN ALONSO MATOS XOL <24470034@camepche.tecnm.mx>
2025-11-21 10:39:57 -0600

    Utiliza el patrón MVVM sin bibliotecas

--- ShopList.Gui/Models/Item.cs
+++ ShopList.Gui/Models/Item.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Maui.Layouts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,6 +14,7 @@ namespace ShopList.Gui.Models
         public string Nombre { get; set; } = string.Empty;
 
         public int Cantidad { get; set; }
+        public bool Comprado { get; set; } =false;
 
         public override string ToString()
         {
